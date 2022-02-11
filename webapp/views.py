9ffae848ac0a7e0283bf4fa3b7b2ae21ca9a2fa0
CyberSecurity0110 <galaxyman0110@gmail.com>
2022-02-11 17:02:58 +0330

    digital_clock

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
+
+def clock(request):
+    now = datetime.now()
+    format ='%H:%M:%S %p'
+    current_time = now.strftime(format)
+    current_date = now.strftime('%d-%m-%Y')
+    context = {'time': current_time, 'date': current_date}
+    return render(request, 'clock.html', context)
